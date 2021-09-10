f5e35b409bd2af427f51821ddcd6ee4744e37b98
dunderrrrrr <emil.bjorkroth@gmail.com>
2021-09-10 19:25:18 +0200

    settings env variables

---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+#############################
+## OPENMEDIAVAULT SETTINGS ##
+#############################
+
+omvuser = os.environ['OPENMEDIAVAULT_USER']
+omvpasswd = os.environ['OPENMEDIAVAULT_PASSWD']
+omvhost = os.environ['OPENMEDIAVAULT_HOST']
+omvport = os.environ['OPENMEDIAVAULT_PORT']
